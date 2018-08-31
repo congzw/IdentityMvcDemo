3602ce8177937baa02a8763b1324bdd04cea28fa
congzw <2981810300@qq.com>
2018-08-31 15:05:21 +0800

    add MultiOwinStartupDemo

---
+++ src/IdentityMvcDemo/App_Start/_MultiOwinStartupDemo.cs
@@ -0,0 +1,26 @@
+﻿using IdentityMvcDemo;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup("StartupFromA", typeof(StartupA))]
+[assembly: OwinStartup("StartupFromB", typeof(StartupB))]
+namespace IdentityMvcDemo
+{
+    public class StartupA
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //<add key="owin:appStartup" value="StartupFromA"/>
+            //just for demo
+        }
+    }
+
+    public class StartupB
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //<add key="owin:appStartup" value="StartupFromB"/>
+            //just for demo
+        }
+    }
+}
